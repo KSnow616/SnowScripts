2fc98ce36bae0b9e2cf96e1ef7865ec1aff380a4
Stuart Li <55991421+KSnow616@users.noreply.github.com>
2022-02-08 15:07:02 -0500

    formatize

--- Layer_Align.jsx
+++ Layer_Align.jsx
@@ -1,65 +1,65 @@
 {
-    function myScript(thisObj){
-       function myScript_buildUI(thisObj){
-          var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "Dockable Script", undefined, {resizeable:true, closeButton: false});
- 
-          res = "group{orientation:'column',\
+   function myScript(thisObj) {
+      function myScript_buildUI(thisObj) {
+         var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "Dockable Script", undefined, { resizeable: true, closeButton: false });
+
+         res = "group{orientation:'column',\
                          groupOne: Group{orientation:'row',\
                          AlignButton: Button{text:'Comp Align'},\
                          Info: Button{text:'?'},\
                 },\
           }";
- 
-          myPanel.grp = myPanel.add(res);
- 
-          //Defaults
+
+         myPanel.grp = myPanel.add(res);
+
+         //Defaults
 
 
 
-          
-          myPanel.grp.groupOne.AlignButton.onClick = function() {
-            try{
+
+         myPanel.grp.groupOne.AlignButton.onClick = function () {
+            try {
                var comp = app.project.activeItem;
                var slLayers = comp.selectedLayers;
                var maxindex = 0;
                for (i = 0; i < slLayers.length; i++) {
-                  if(slLayers[i].index>maxindex){
-                     maxindex=slLayers[i].index;
+                  if (slLayers[i].index > maxindex) {
+                     maxindex = slLayers[i].index;
                   }
                }
 
                for (i = 0; i < slLayers.length; i++) {
-                  slLayers[i].inPoint = comp.layer(maxindex+1).inPoint
-                  slLayers[i].outPoint = comp.layer(maxindex+1).outPoint
+                  slLayers[i].inPoint = comp.layer(maxindex + 1).inPoint
+                  slLayers[i].outPoint = comp.layer(maxindex + 1).outPoint
                }
-            }catch(error){
-                alert("Please select a layer.")
+            } catch (error) {
+               alert("Please select a layer.")
             }
 
-              }
-          
-          myPanel.grp.groupOne.Info.onClick = function() {
-              alert("Align and Trim a layer (or layers) to its beneath layer. v.0.1.0 By KSnow.")
-          }
-          myPanel.grp.groupOne.Info.size = [25, 25];
-          myPanel.layout.layout(true);
- 
-          return myPanel;
-       }
+         }
+
+         myPanel.grp.groupOne.Info.onClick = function () {
+            alert("Align and Trim a layer (or layers) to its beneath layer. v.0.1.0 By KSnow.")
+         }
+         myPanel.grp.groupOne.Info.size = [25, 25];
+         myPanel.layout.layout(true);
+
+         return myPanel;
+      }
 
 
-    
-    
-       var myScriptPal = myScript_buildUI(thisObj);
- 
-       if (myScriptPal != null && myScriptPal instanceof Window){
-          myScriptPal.center();
-          myScriptPal.show();
-       }
- 
-    }
-    myScript(this);
- }
- 
+
+
+      var myScriptPal = myScript_buildUI(thisObj);
+
+      if (myScriptPal != null && myScriptPal instanceof Window) {
+         myScriptPal.center();
+         myScriptPal.show();
+      }
+
+   }
+   myScript(this);
+}
+
 
 
